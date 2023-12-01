7d9b823de3bd464c73a89a8894871640a25a743c
Brian Han <brihan.tech@gmail.com>
2023-11-30 16:03:53 -0800

    refactor: follow pep8

--- src/encoder.py
+++ src/encoder.py
@@ -1,22 +1,21 @@
-#!/usr/bin/python3
+"""
+!/usr/bin/python3
 
-#Python library to interface with the chip LS7366R for the Raspberry Pi
-#Written by Federico Bolanos
-#Last Edit: March 4th 2014
-
+# Python library to interface with the chip LS7366R for the Raspberry Pi
+# Written by Federico Bolanos
+# Refactored by Brian Han
+# Last Edit: Nov 27th 2023
+"""
 import spidev
 from time import sleep
 
 
-#Usage: import classReadEncoder then create an object by calling obj.Encoder(CSX, CLK, BTMD)
-#e.g. lever.Encoder(0, 1000000, 4)
-
 class Encoder():
-
-    #-------------------------------------------
-    # Constants
-
-    #   Commands
+    """
+    Usage: import encoder then create an object by calling obj.Encoder(csx, clk, btmd)
+    e.g. lever.Encoder(0, 1000000, 4)
+    """
+    # Commands
     CLEAR_COUNTER = 0x20
     CLEAR_STATUS = 0x30
     READ_COUNTER = 0x60
@@ -24,7 +23,7 @@ class Encoder():
     WRITE_MODE0 = 0x88
     WRITE_MODE1 = 0x90
 
-    #   Modes
+    # Modes
     NQ_COUNT = 0x00
     ONEX_COUNT = 0x01
     TWOX_COUNT = 0x02
@@ -37,67 +36,56 @@ class Encoder():
 
     BYTE_MODE = [ONEBYTE_COUNTER, TWOBYTE_COUNTER, THREEBYTE_COUNTER, FOURBYTE_COUNTER]
 
-    #   Values
+    # Variables
     max_val = 4294967295
-    
-    # Global Variables
+    counter_size = 4 #Default 4
 
-    counterSize = 4 #Default 4
-    
-    #----------------------------------------------
-    # Constructor
+    def __init__(self, csx, clk, btmd):
+        self.counter_size = btmd # Sets the byte mode that will be used
 
-    def __init__(self, CSX, CLK, BTMD):
-        self.counterSize = BTMD #Sets the byte mode that will be used
+        self.spi = spidev.SpiDev() # Initialize object
+        self.spi.open(0, csx) # Which CS line will be used
+        self.spi.max_speed_hz = clk # Speed of clk (modifies speed transaction)
 
-
-        self.spi = spidev.SpiDev() #Initialize object
-        self.spi.open(0, CSX) #Which CS line will be used
-        self.spi.max_speed_hz = CLK #Speed of clk (modifies speed transaction)
-
-        #Init the Encoder
-        print('Clearing Encoder CS%s\'s Count...\t' % (str(CSX)), self.clearCounter())
-        print('Clearing Encoder CS%s\'s Status..\t' % (str(CSX)), self.clearStatus())
+        # Init the Encoder
+        print(f'Clearing Encoder CS{csx}\'s Count...\t', self.clear_counter())
+        print(f'Clearing Encoder CS{csx}\'s Status..\t', self.clear_status())
 
         self.spi.xfer2([self.WRITE_MODE0, self.ONEX_COUNT])
-        
-        sleep(.1) #Rest
-        
-        self.spi.xfer2([self.WRITE_MODE1, self.BYTE_MODE[self.counterSize-1]])
+
+        sleep(.1) # Rest
+
+        self.spi.xfer2([self.WRITE_MODE1, self.BYTE_MODE[self.counter_size-1]])
 
     def close(self):
         print('\nThanks for using me! :)')
         self.spi.close()
 
-    def clearCounter(self):
+    def clear_counter(self):
         self.spi.xfer2([self.CLEAR_COUNTER])
-
         return '[DONE]'
 
-    def clearStatus(self):
+    def clear_status(self):
         self.spi.xfer2([self.CLEAR_STATUS])
-
         return '[DONE]'
 
-    def readCounter(self):
-        readTransaction = [self.READ_COUNTER]
+    def read_counter(self):
+        read_transaction = [self.READ_COUNTER]
 
-        for i in range(self.counterSize):
-            readTransaction.append(0)
-            
-        data = self.spi.xfer2(readTransaction)
+        for i in range(self.counter_size):
+            read_transaction.append(0)
 
-        EncoderCount = 0
-        for i in range(self.counterSize):
-            EncoderCount = (EncoderCount << 8) + data[i+1]
+        data = self.spi.xfer2(read_transaction)
 
-        if data[1] != 255:    
-            return EncoderCount
-        else:
-            return (EncoderCount - (self.max_val+1))  
-        
-    def readStatus(self):
+        encoder_count = 0
+        for i in range(self.counter_size):
+            encoder_count = (encoder_count << 8) + data[i+1]
+
+        if data[1] != 255:
+            return encoder_count
+
+        return encoder_count - (self.max_val+1)
+
+    def read_status(self):
         data = self.spi.xfer2([self.READ_STATUS, 0xFF])
-        
         return data[1]
-
