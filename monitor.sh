9f3f03cf7155c54d38c195bb2f1185a8c5d3da39
Brian Han <brihan.tech@gmail.com>
2023-12-01 14:35:11 -0800

    feat: updated code after testing

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/pi/piPaw/
+cd /home/pi/PiPaw/
 until python3 -u src/piPaw_amplitude.py &>>log.txt; do
 	echo "'piPaw program' crashed with exit code $?. Restarting..." >&2
 	sleep 1
