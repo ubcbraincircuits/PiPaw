597589b29874c3adacbdf15ad8fcd25c83f00ec1
Brian Han <brihan.tech@gmail.com>
2023-12-01 09:12:43 -0800

    feat: update `monitor.sh` location and path for `piPaw`

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/pi/piPaw/
-until python3 -u piPaw_amplitude.py &>>log.txt; do
+until python3 -u src/piPaw_amplitude.py &>>log.txt; do
 	echo "'piPaw program' crashed with exit code $?. Restarting..." >&2
 	sleep 1
 done
