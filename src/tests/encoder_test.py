9a8691af6175e9222c57c637bf66dcb73382bef0
Brian Han <brihan.tech@gmail.com>
2023-11-30 16:07:15 -0800

    feat: update test cases

--- src/tests/encoder_test.py
+++ src/tests/encoder_test.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
+import time
+from src.encoder import Encoder
 
-import src.encoder
-from time import sleep
-from sys import stdout
-
-#Settins for Right Encoder class:
-CSX = 0
-CLK = 1000000
-BYTEMD = 4
-
-lever = classReadEncoder.Encoder(CSX,CLK,BYTEMD)
-
-
-#start = time()
-#data = []
-#freq = 0
-
-while True:
+def main():
     try:
-        print('Right Counter reads [%s].' % str(lever.readCounter()), end='\r', flush=True)
-        sleep(0.1)
-        #print str(rLever.readCounter())
-        #print 'CNTRSIZE = ', str(rLever.counterSize)
-        #data.append(rLever.readCounter())
-        #if time()-start > 10:
-            #print '60 seconds passed.'
-            #print data
-            #print str(len(data))
-            #break
-    except KeyboardInterrupt:
-        lever.close()
-        break
+        # Settings for Right Encoder class
+        CSX = 0
+        CLK = 1000000
+        BYTEMD = 4
 
-print('Cya!')
+        lever = Encoder(CSX, CLK, BYTEMD)
+
+        while True:
+            try:
+                counter_value = lever.read_counter()
+                print(f'Right Counter reads [{counter_value}].', end='\r', flush=True)
+                time.sleep(0.1)
+            except KeyboardInterrupt:
+                break
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    finally:
+        if 'lever' in locals():
+            lever.close()
+        print('Goodbye!')
+
+if __name__ == "__main__":
+    main()
